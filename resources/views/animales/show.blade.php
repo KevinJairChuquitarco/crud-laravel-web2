95dcacd1e264bef1a60456235e7613da604961b3
KevinJairChuquitarco <kevinchuquitarco11@gmail.com>
2025-01-21 08:47:54 -0500

    imagen url listo

--- resources/views/animales/show.blade.php
+++ resources/views/animales/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Detalle de animal</h1>
-    <img src="{{ $animal->url }}" alt="Imagen animal">
+    <img src="{{ $animal->url }}" alt="Imagen animal" class="img-fluid">
     <p><strong>Nombre:</strong> {{ $animal->nombre }}</p>
     <p><strong>Especie:</strong> {{ $animal->especie }}</p>
 @endsection
